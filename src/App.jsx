18b5df05cdacdee581b6c80848fcc67158b8f72f
SFSF <55688990+sohei-fujita@users.noreply.github.com>
2024-03-09 09:54:57 +0000

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { useState, useCallback } from "react";
 import "./styles.css";
-import { ChildArea } from "./ChildArea";
-import { CssModules } from "./components/CssModules";
-import { StyledJsx } from "./components/StyledJsx";
-import { Emotion } from "./components/Emotion";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 
 export default function App() {
   const [text, setText] = useState("");
@@ -20,15 +20,25 @@ export default function App() {
   const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
-    <div className="App">
-      <Emotion />
-      <CssModules />
-      <StyledJsx />
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/page1">
+          <Page1 />
+        </Route>
+        <Route exact path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
