72ced0abe3baac35a6da9f5810d0e1646fbe5cd9
SFSF <55688990+sohei-fujita@users.noreply.github.com>
2024-03-11 09:43:46 +0000

    usHistory

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,15 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1Detail1A = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
+  console.log(location);
   return (
     <div>
       <h1>Page1Detail1Aページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
