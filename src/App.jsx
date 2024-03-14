f34c35c0add88ef924c269bde72d7f04508b2b20
SFSF <55688990+sohei-fujita@users.noreply.github.com>
2024-03-14 12:38:35 +0000

    molecures

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { useState, useCallback } from "react";
 import { Router } from "./router/Router";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecures/SearchInput";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
