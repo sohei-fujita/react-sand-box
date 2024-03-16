5b755adcd0e802f09835576ffe9d48c3fcac1889
SFSF <55688990+sohei-fujita@users.noreply.github.com>
2024-03-16 15:13:18 +0000

    UserList

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,10 @@ import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecures/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
-
-const user = {
-  name: "fujita",
-  image:
-    "https://source.unsplash.com/brown-short-coated-dog-in-orange-hoodie-oU6KZTXhuvk",
-  email: "12345@example.com",
-  phone: "090-11111-1111",
-  company: {
-    name: "test company",
-  },
-  website: "https://google.com",
-};
+import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
